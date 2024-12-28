daabc0bce753acc545efcbdb0c3e6e49a5219420
redjax <no@none.com>
2024-12-27 20:54:12 -0500

    Add alembic ini file.
    Add simple cyclopts CLI

---
+++ applications/cli/src/weather_cli/main.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import typing as t
+import sys
+
+from loguru import logger as log
+from setup import setup_loguru_logging
+
+from cyclopts import App, Group, Parameter
+
+from loguru import logger as log
+
+app = App(name="weathercli", help="CLI for WeatherData app.")
+
+app.meta.group_parameters = Group("Session Parameters", sort_key=0)
+
+## Mount apps
+# app.command(<app-name>)
+
+@app.meta.default
+def cli_launcher(*tokens: t.Annotated[str, Parameter(show=False, allow_leading_hyphen=True)], debug: bool = False):
+    log.remove(0)
+    
+    if debug:
+        log.add(sys.stderr, format="{time:YYYY-MM-DD HH:mm:ss} | [{level}] | {name}.{function}:{line} |> {message}", level="DEBUG")
+        
+        log.debug("CLI debugging enabled.")
+    else:
+        log.add(sys.stderr, format="{time:YYYY-MM-DD HH:mm:ss} [{level}] : {message}", level="INFO")
+                
+    app(tokens)
+
+if __name__ == "__main__":
+    app()
