f65b8f63820914113bde3c80fe9dc34a0a8f7167
redjax <no@none.com>
2024-12-27 22:08:10 -0500

    Add forecast to CLI

--- applications/cli/src/weather_cli/weather.py
+++ applications/cli/src/weather_cli/weather.py
@@ -40,7 +40,7 @@ def get_current_weather(location: t.Annotated[str, Parameter(name="location", sh
     log.debug(f"Current weather schema: {current_weather}")
 
     current_weather_str: str = f"""
-[ '{location.name}, {location.region} ({location.country})' ]
+[ Current Weather for: '{location.name}, {location.region} ({location.country})' ]
 
 Temperature: {current_weather.heatindex_f} °F
 Feels Like: {current_weather.feelslike_f} °F
@@ -57,4 +57,39 @@ Wind:
     Degree: {current_weather.wind_degree} degrees
 """
 
-    log.info(f"Current weather:\n{current_weather_str}")
+    log.info(current_weather_str)
+    
+
+@weather_app.command(name="forecast")
+def get_weather_forecast(location: t.Annotated[str, Parameter(name="location", show_default=True)] = "London"):
+    """Get the weather forecast for a given location.
+    
+    Params:
+        location: The location to get the weather forecast for. Can be a city name or postal code.
+    """
+    log.info(f"Getting weather forecast for location: {location}")
+    
+    try:
+        forecast_res = api_weatherapi.client.get_weather_forecast(location=location)
+    except Exception as exc:
+        msg = f"({type(exc)}) Error getting weather forecast for location '{location}'. Details: {exc}"
+        log.error(msg)
+        
+        exit(1)
+        
+    log.debug(f"Weather forecast response: {forecast_res}")
+        
+    location: location_domain.LocationIn = location_domain.LocationIn.model_validate(forecast_res["location"])
+    log.debug(f"Location schema: {location}")
+    
+#     current_weather: weather_domain.current.CurrentWeatherIn = weather_domain.current.CurrentWeatherIn.model_validate(current_weather_res["current"])
+    forecast_json: weather_domain.forecast.ForecastJSONIn = weather_domain.forecast.ForecastJSONIn(forecast_json=forecast_res["forecast"])
+    log.debug(f"Weather forecast schema: {forecast_json}")
+    
+    weather_forecast_str: str = f"""
+[ Weather forecast for: '{location.name}, {location.region} ({location.country})' ]
+
+[NotImplemented]
+"""
+
+    log.info(weather_forecast_str)
