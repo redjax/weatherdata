4369f11da260731d3644c88b65cec7ffc047a02f
redjax <no@none.com>
2024-12-28 21:49:20 -0500

    Setup redis, rabbitmq, dramatiq.
    Dramatiq isn't working, but the pika connection test is... 🤔

--- packages/settings/src/settings/dramatiq_settings.py
+++ packages/settings/src/settings/dramatiq_settings.py
@@ -16,19 +16,34 @@ DRAMATIQ_SETTINGS: Dynaconf = Dynaconf(
 
 def return_dramatiq_rabbitmq_url(
     username: str = DRAMATIQ_SETTINGS.get("DRAMATIQ_RABBITMQ_USERNAME", default="guest"),
-    password: str = DRAMATIQ_SETTINGS.get("DRAMATIQ_RABBITMQ_PASSWORD", default=""),
+    password: str | None = DRAMATIQ_SETTINGS.get("DRAMATIQ_RABBITMQ_PASSWORD", default=""),
     host: str = DRAMATIQ_SETTINGS.get("DRAMATIQ_RABBITMQ_HOST", default="localhost"),
     port: str = DRAMATIQ_SETTINGS.get("DRAMATIQ_RABBITMQ_PORT", default=5672),
-):
-    """Return a string of the format amqp://<username>:<password>@<host>:<port> for RabbitMQ.
+    vhost: str | None = DRAMATIQ_SETTINGS.get("DRAMATIQ_RABBITMQ_VHOST", default="rabbitmq")
+) -> str:
+    """Return a string of the format amqp://<username>:<password>@<host>:<port> (or amqp://<username>@<host>:<port> for guest/passwordless auth) for RabbitMQ.
     
     Params:
         username (str): The username for database auth.
-        password (str): The password for database auth.
-        host (str): The host for database auth.
+        password (str | None): The password for database auth.
+        host (str | None): The host for database auth.
         port (str): The port for database auth.
+        vhost (str | None): The vhost for database auth.
+        
+    Returns:
+        (str): A string of the format amqp://<username>:<password>@<host>:<port>/<vhost>
+    
     """
-    dramatiq_broker_url: str = f"amqp://{username}:{password}@{host}:{port}"
+    if password:
+        dramatiq_broker_url: str = f"amqp://{username}:{password}@{host}:{port}"
+    else:
+        dramatiq_broker_url: str = f"amqp://{username}:''@{host}:{port}"
+        
+    if vhost:
+        dramatiq_broker_url += f"/{vhost}"
+    else:
+        dramatiq_broker_url += "/"
+
     log.debug(f"Dramatiq broker URL: {dramatiq_broker_url}")
     
     return dramatiq_broker_url
--- sandbox/task_scheduler/scheduler.py
+++ sandbox/task_scheduler/scheduler.py
@@ -17,12 +17,29 @@ dramatiq_rabbitmq_settings = {
     "credentials": pika.PlainCredentials(
         DRAMATIQ_SETTINGS.get("DRAMATIQ_RABBITMQ_USERNAME", default="guest"),
         DRAMATIQ_SETTINGS.get("DRAMATIQ_RABBITMQ_PASSWORD", default="")
-)
+    )
 }
 
-# dramatiq_broker_url = f"amqp://{dramatiq_rabbitmq_settings['user']}:{dramatiq_rabbitmq_settings['password']}@{dramatiq_rabbitmq_settings['host']}:{dramatiq_rabbitmq_settings['port']}"
-
 demo_db_dict = {"drivername": "sqlite+pysqlite", "username": None, "password": None, "host": None, "port": None, "database": ".db/demo.sqlite3"}
+
+
+def test_rabbitmq_connection(username: str = "rabbitmq", password: str = "rabbitmq", host: str = "localhost", port: int = 5672, vhost: str = "rabbitmq"):
+    ## Test connection with pika
+    credentials = pika.PlainCredentials(username, password)
+    parameters = pika.ConnectionParameters(host, port, vhost, credentials)
+    
+    log.debug(f"""
+RabbitMQ Connection Test
+Username: {username}
+Password: {password}
+VHOST: {vhost}
+""")
+
+    try:
+        connection = pika.BlockingConnection(parameters)
+        log.success("Connection successful!")
+    except Exception as e:
+        log.error(f"Connection failed: {e}")
 
 if __name__ == "__main__":
     setup.setup_loguru_logging(log_level=LOGGING_SETTINGS.get("LOG_LEVEL", default="INFO"), colorize=True)
@@ -31,13 +48,27 @@ if __name__ == "__main__":
     demo_db_uri = depends.get_db_uri(**demo_db_dict)
     setup.setup_database(engine=depends.get_db_engine(db_uri=demo_db_uri))
     
-    log.debug(f"Test dramatiq rabbitmq URL: {return_dramatiq_rabbitmq_url()}")
-    log.debug(f"Test dramatiq rabbitmq pika credentials: {return_dramatiq_rabbitmq_credentials()}")
+    test_rabbitmq_connection(username=DRAMATIQ_SETTINGS.DRAMATIQ_RABBITMQ_USERNAME, password=DRAMATIQ_SETTINGS.DRAMATIQ_RABBITMQ_PASSWORD, vhost=DRAMATIQ_SETTINGS.DRAMATIQ_RABBITMQ_VHOST)
+    
+    # log.debug(f"Username: {DRAMATIQ_SETTINGS.DRAMATIQ_RABBITMQ_USERNAME}")
+    # log.debug(f"Password: {DRAMATIQ_SETTINGS.DRAMATIQ_RABBITMQ_PASSWORD}")
+    # log.debug(f"VHOST: {DRAMATIQ_SETTINGS.DRAMATIQ_RABBITMQ_VHOST}")
+    
+    # log.debug(f"Test dramatiq rabbitmq URL: {return_dramatiq_rabbitmq_url()}")
+    # log.debug(f"Test dramatiq rabbitmq pika credentials: {return_dramatiq_rabbitmq_credentials()}")
+    
+    dramatiq_broker_url = return_dramatiq_rabbitmq_url()
+    
+    # log.debug(f"Dramatiq RabbitMQ settings: {dramatiq_rabbitmq_settings}")
+    log.debug(f"Dramatiq broker URL: {dramatiq_broker_url}")
     
     # rabbitmq_broker = RabbitmqBroker(**dramatiq_rabbitmq_settings)
-    # rabbitmq_broker = RabbitmqBroker(url=dramatiq_broker_url)
+    rabbitmq_broker = RabbitmqBroker(url=dramatiq_broker_url)
     
-    log.debug(f"Dramatiq RabbitMQ settings: {dramatiq_rabbitmq_settings}")
-    # log.debug(f"Dramatiq broker URL: {dramatiq_broker_url}")
-    
-    # count_words.send("https://www.xkcd.com")
+    try:
+        count_words.send("https://www.xkcd.com")
+    except Exception as exc:
+        msg = f"({type(exc)}) Unhandled exception counting words at https://www.xkcd.com. Details: {exc}"
+        log.error(msg)
+        
+        raise exc
