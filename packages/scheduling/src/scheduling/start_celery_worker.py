4962bbfee859e60f8017303ba4d8f44920c35ab7
redjax <no@none.com>
2024-12-30 01:23:15 -0500

    Add logging from sandbox entrypoint

--- packages/scheduling/src/scheduling/start_celery_worker.py
+++ packages/scheduling/src/scheduling/start_celery_worker.py
@@ -18,6 +18,7 @@ def run():
         ]
     )
 
+    log.info("Starting Celery worker")
     try:
         worker = celeryapp.app.worker_main(
             argv=["worker", "--loglevel=DEBUG", "--uid=0", "--gid=0"]
@@ -29,24 +30,8 @@ def run():
 
         raise exc
     
-    if worker is None:
-        raise ValueError(f"Celery app.worker_main() returned None. This could be an issue authenticating to the broker/backend. Check the Celery logs for details.")
-
-    log.info("Starting Celery worker")
-    try:
-        worker.start()
-    except Exception as exc:
-        msg = Exception(f"Unhandled exception starting Celery worker. Details: {exc}")
-        log.error(msg)
-        log.trace(exc)
-
-        raise exc
-
-# def run():
-#     subprocess.run(["celery", "-A", "scheduling.celery_scheduler.celeryapp", "worker", "--loglevel=DEBUG"])
-
-# def run_beat():
-#     subprocess.run(["celery", "-A", "scheduling.celery_scheduler.celeryapp", "beat", "--loglevel=DEBUG"])
+    # if worker is None:
+    #     raise ValueError(f"Celery app.worker_main() returned None. This could be an issue authenticating to the broker/backend. Check the Celery logs for details.")
 
 if __name__ == "__main__":
     setup.setup_loguru_logging(log_level=LOGGING_SETTINGS.get("LOG_LEVEL", default="INFO"), colorize=True)
--- sandbox/task_scheduler/celery_beat.py
+++ sandbox/task_scheduler/celery_beat.py
@@ -2,13 +2,28 @@ from settings.celery_settings import CELERY_SETTINGS
 from settings.logging_settings import LOGGING_SETTINGS
 from loguru import logger as log
 
+import db
+import depends
 import setup
 
 from scheduling import start_celery_beat
 
+demo_db_dict = {"drivername": "sqlite+pysqlite", "username": None, "password": None, "host": None, "port": None, "database": ".db/demo.sqlite3"}
+
+def demo_db_engine(db_conf: dict = demo_db_dict, echo: bool = False):
+    db_uri = db.get_db_uri(**db_conf)
+    engine = db.get_engine(url=db_uri, echo=echo)
+    
+    return engine
+
 if __name__ == "__main__":
     setup.setup_loguru_logging(log_level=LOGGING_SETTINGS.get("LOG_LEVEL", default="INFO"), colorize=True)
-    setup.setup_database()
+    log.add("sandbox/task_scheduler/logs/celery_beat.log", retention=3, rotation="15 MB", level="DEBUG")
+    log.add("sandbox/task_scheduler/logs/celery_beat_error.log", retention=3, rotation="15 MB", level="ERROR")
+    
+    ## Create a demo.sqlite3 database separate from app's database
+    demo_db_uri = depends.get_db_uri(**demo_db_dict)
+    setup.setup_database(engine=depends.get_db_engine(db_uri=demo_db_uri))
     
     log.debug(f"Celery settings: {CELERY_SETTINGS.as_dict()}")
     
