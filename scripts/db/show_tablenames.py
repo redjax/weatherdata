41734b2365efdbac69c77b5677d77e5f97d1d578
redjax <no@none.com>
2025-01-04 23:33:20 -0500

    Add script to show database table names

---
+++ scripts/db/show_tablenames.py
@@ -0,0 +1,16 @@
+from loguru import logger as log
+import setup
+import settings
+import db
+import depends
+
+if __name__ == "__main__":
+    setup.setup_loguru_logging(log_level=settings.LOGGING_SETTINGS.get("LOG_LEVEL", default="INFO"), colorize=True)
+    setup.setup_database()
+    
+    tables = db.show_table_names(engine=depends.get_db_engine())
+    
+    log.debug(f"Found [{len(tables)}] table(s) in the database.")
+    print(f"\nTables [{len(tables)}]:")
+    for table in tables:
+        print(f"  - {table}")
