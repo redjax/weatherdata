b5e117dc8b13b62b8170541acd04de591b2d534c
redjax <no@none.com>
2025-03-02 02:21:08 -0500

    Add config, direnv setup CLI commands

--- applications/cli/src/weather_cli/main.py
+++ applications/cli/src/weather_cli/main.py
@@ -6,6 +6,7 @@ import typing as t
 from .celery import celery_app
 from .db import db_app
 from .weather import weather_app
+from .setup import setup_app
 
 from cyclopts import App, Group, Parameter
 from loguru import logger as log
@@ -14,7 +15,7 @@ app = App(name="weathercli", help="CLI for WeatherData app.")
 
 app.meta.group_parameters = Group("Session Parameters", sort_key=0)
 
-MOUNT_SUB_CLIS: list = [weather_app, celery_app, db_app]
+MOUNT_SUB_CLIS: list = [weather_app, celery_app, db_app, setup_app]
 
 ## Mount apps
 for sub_cli in MOUNT_SUB_CLIS:
