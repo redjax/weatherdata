18dd1c95a49f21625e373dad3eb7a7fa9f22d09f
redjax <no@none.com>
2025-04-07 20:29:54 -0400

    Start work on OpenMeteo schemas

--- packages/domain/src/domain/openmeteo/weather/current/schemas.py
+++ packages/domain/src/domain/openmeteo/weather/current/schemas.py
@@ -13,4 +13,79 @@ from pydantic import (
     field_validator,
 )
 
-__all__ = []
+__all__ = [
+    "MeteoCurrentWeatherIn",
+    "MeteoCurrentWeatherOut",
+    "MeteoCurrentWeatherUnitsIn",
+    "MeteoCurrentWeatherUnitsOut",
+    "MeteoCurrentIn",
+    "MeteoCurrentOut",
+]
+
+
+class MeteoCurrentWeatherUnitsIn(BaseModel):
+    time: str
+    interval: str
+    temperature_2m: str
+    relative_humidity_2m: str
+    apparent_temperature: str
+    is_day: str | None = Field(default=None)
+    precipitation: str
+    rain: str
+    showers: str
+    snowfall: str
+    weather_code: str
+    cloud_cover: str
+    pressure_msl: str
+    surface_pressure: str
+    wind_speed_10m: str
+    wind_direction_10m: str
+    wind_gusts_10m: str
+
+
+class MeteoCurrentWeatherUnitsOut(MeteoCurrentWeatherUnitsIn):
+    id: int
+
+
+class MeteoCurrentIn(BaseModel):
+    time: str
+    interval: int
+    temperature_2m: Decimal
+    relative_humidity_2m: Decimal
+    apparent_temperature: Decimal
+    is_day: int
+    precipitation: Decimal
+    rain: Decimal
+    showers: Decimal
+    snowfall: Decimal
+    weather_code: int
+    cloud_cover: int
+    pressure_msl: Decimal
+    surface_pressure: Decimal
+    wind_speed_10m: Decimal
+    wind_direction_10m: Decimal
+    wind_gusts_10m: Decimal
+
+
+class MeteoCurrentOut(MeteoCurrentIn):
+    id: int
+
+
+## Main class
+
+
+class MeteoCurrentWeatherIn(BaseModel):
+    latitude: Decimal
+    longitude: Decimal
+    generationtime_ms: Decimal
+    utc_offset_seconds: Decimal
+    timezone: str
+    timezone_abbreviation: str
+    elevation: Decimal
+    current_units: MeteoCurrentWeatherUnitsIn
+
+
+class MeteoCurrentWeatherOut(MeteoCurrentWeatherIn):
+    id: int
+
+    current_units: MeteoCurrentWeatherUnitsOut
