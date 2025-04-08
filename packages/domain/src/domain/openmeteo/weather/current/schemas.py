c00613f45d9f3599ab4d7f1612814d0cd29732e8
redjax <no@none.com>
2025-04-07 20:36:58 -0400

    Continue building Meteo current weather schemas

--- packages/domain/src/domain/openmeteo/weather/current/schemas.py
+++ packages/domain/src/domain/openmeteo/weather/current/schemas.py
@@ -71,6 +71,54 @@ class MeteoCurrentOut(MeteoCurrentIn):
     id: int
 
 
+class MeteoMinutely15UnitsIn(BaseModel):
+    time: str
+    temperature_2m: str
+    precipitation: str
+    weather_code: str
+    wind_direction_80m: str
+    is_day: str | None = Field(default=None)
+    relative_humidity_2m: str
+    rain: str
+    wind_gusts_10m: str
+    wind_speed_10m: str
+    dew_point_2m: str
+    snowfall: str
+    wind_speed_80m: str
+    visibility: str
+    apparent_temperature: str
+    sunshine_duration: str
+    wind_direction_10m: str
+
+
+class MeteoMinutely15UnitsOut(BaseModel):
+    id: int
+
+
+class MeteoMinutely15In(BaseModel):
+    time: list[str] = Field(default_factory=list)
+    temperature_2m: list[str] = Field(default_factory=list)
+    precipitation: list[Decimal] = Field(default_factory=list)
+    weather_code: list[int] = Field(default_factory=list)
+    wind_direction_80m: list[int] = Field(default_factory=list)
+    is_dat: list[int] = Field(default_factory=list)
+    relative_humidity_2m: list[int] = Field(default_factory=list)
+    rain: list[Decimal] = Field(default_factory=list)
+    wind_gusts_10m: list[Decimal] = Field(default_factory=list)
+    wind_speed_10m: list[Decimal] = Field(default_factory=list)
+    dew_point_2m: list[Decimal] = Field(default_factory=list)
+    snowfall: list[Decimal] = Field(default_factory=list)
+    wind_speed_80m: list[Decimal] = Field(default_factory=list)
+    visibility: list[Decimal] = Field(default_factory=list)
+    apparent_temperature: list[Decimal] = Field(default_factory=list)
+    sunshine_duration: list[Decimal] = Field(default_factory=list)
+    wind_direction_10m: list[int] = Field(default_factory=list)
+
+
+class MeteoMinutely15UnitsOut(MeteoMinutely15In):
+    id: int
+
+
 ## Main class
 
 
@@ -83,9 +131,12 @@ class MeteoCurrentWeatherIn(BaseModel):
     timezone_abbreviation: str
     elevation: Decimal
     current_units: MeteoCurrentWeatherUnitsIn
+    current: MeteoCurrentIn
+    minutely_15_units: MeteoMinutely15UnitsIn
 
 
 class MeteoCurrentWeatherOut(MeteoCurrentWeatherIn):
     id: int
 
     current_units: MeteoCurrentWeatherUnitsOut
+    current: MeteoCurrentOut
